f8e8c6dab4ffd2b850f48476b4f4ba4d9732ab65
HPJ <klps2146@gmail.com>
2025-06-09 23:10:06 +0800

    mod

--- Scene/PlayScene.hpp
+++ Scene/PlayScene.hpp
@@ -25,12 +25,8 @@ public: //// new 本來是 privte
         TILE_WATER,
         TILE_GRASS,
         TILE_ROCK,
-<<<<<<< HEAD
-        TILE_OCCUPIED
-=======
         TILE_OCCUPIED,
         TILE_BRIDGE,
->>>>>>> 355c22250c1faa69e9be410eee38469ffd593de0
     };
     ALLEGRO_SAMPLE_ID bgmId;
     std::shared_ptr<ALLEGRO_SAMPLE_INSTANCE> deathBGMInstance;
